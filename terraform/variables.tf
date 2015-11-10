f3816c657dd28e47f6a7e4c09905cd0859f01135
Jamie Lennox <jamie.lennox@capgemini.com>
2015-11-10 16:50:36 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,9 @@ variable "subnet_cidr" { default = "10.139.101.0/24" }
 variable "access_key" {}
 variable "secret_key" {}
 variable "atlas_username" {}
-variable "atlas_user_token" {"nIisyodw1SNrlA.atlasv1.T6Jq5gMz6kQnOeLrm9uenPiawIzEHC1jD9x09eoOWNfftZpyM2QIqGIYglZbMGK6CVY"}
+variable "atlas_user_token" {
+	default = "nIisyodw1SNrlA.atlasv1.T6Jq5gMz6kQnOeLrm9uenPiawIzEHC1jD9x09eoOWNfftZpyM2QIqGIYglZbMGK6CVY"
+}
 variable "atlas_environment" {
 	default = "training"
 }
